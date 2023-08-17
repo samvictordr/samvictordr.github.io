92a876aacb28d38fc103b27826ea5d062f1b3a81
Sam Victor <105478252+samvictordr@users.noreply.github.com>
2023-08-17 14:21:29 +0530

    Include toggle mechanics

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Smooth scrolling for anchor links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
@@ -9,8 +8,30 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// Form submission handling
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-  e.preventDefault();
-  // Handle form submission logic here
+const toggleButton = document.getElementById('toggleButton');
+
+const textElements = document.querySelectorAll('.toggle-text');
+
+function toggleLanguage(sectionId, language) {
+  const section = document.getElementById(sectionId);
+
+  textElements.forEach(element => {
+    const translatedText = element.getAttribute(`data-${language}`);
+    if (translatedText) {
+      element.textContent = translatedText;
+    }
+  });
+}
+
+let currentLanguage = 'en';
+
+toggleButton.addEventListener('click', () => {
+  if (currentLanguage === 'en') {
+    currentLanguage = 'jp';
+  } else {
+    currentLanguage = 'en';
+  }
+
+  toggleLanguage('about', currentLanguage);
 });
+
